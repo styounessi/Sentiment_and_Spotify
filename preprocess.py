b400ae077eca17493e5bb7fa0887f1c6a09e0234
Steve Younessi <63491724+styounessi@users.noreply.github.com>
2023-02-04 12:10:58 -0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -5,13 +5,17 @@ import pandas as pd
 import lyricsgenius as genius
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
 
-#----------------------------------------------------------------#
+#-----------------------Get Album via Spotify API-----------------------#
 
-sp_client_id = open('.client_id', 'r')
-sp_client_secret = open('.client_secret', 'r')
+# Read Spotify client_id and client_secret keys
+with open('.client_id', 'r') as f:
+    sp_client_id = f.read().strip()
 
+with open('.client_secret', 'r') as f:
+    sp_client_secret = f.read().strip()
+
+# Connect to Spotify and post request
 auth_url = 'https://accounts.spotify.com/api/token'
-
 auth_req = requests.post(auth_url, {'grant_type': 'client_credentials',
                                     'client_id': sp_client_id,
                                     'client_secret': sp_client_secret})
@@ -19,31 +23,34 @@ auth_req = requests.post(auth_url, {'grant_type': 'client_credentials',
 auth_resp = auth_req.json()
 sp_token = auth_resp['access_token']
 
-headers = {'Authorization': 'Bearer {token}'.format(token=sp_token)}
+headers = {'Authorization': f'Bearer {sp_token}'}
 
+# Get specific album with tracks
 get_album = requests.get('https://api.spotify.com/v1/albums/6NTrwu1XJ56jBPx7HMksbZ/tracks',
                          headers=headers)
 
-#----------------------------------------------------------------#
+#-----------------------Get Spotify Audio Features-----------------------#
 
 get_tracks = get_album.json()['items']
 
-track_feat = []
+# Grab track titles and rename to remove unnecessary text
+track_names = [track['name'] for track in get_tracks]
+track_names = [name.replace('- 2015 Remaster', '') for name in track_names]
 
-for track in get_tracks:
-    features = requests.get('https://api.spotify.com/v1/audio-features/' + track['id'],
-                            headers=headers)
-    features = features.json()
-    features.update({'track_name': track['name']})
-    track_feat.append(features)
+track_ids = [track['id'] for track in get_tracks]
+track_feat = [requests.get(f'https://api.spotify.com/v1/audio-features/{id}',
+                           headers=headers).json() for id in track_ids]
 
+for track, name in zip(track_feat, track_names):
+    track.update({'track_name': name})
+
+# Convert to DataFrame and only keep a few audio features to append with later
 album_feat = pd.DataFrame(track_feat)
 album_feat = album_feat[['track_name', 'danceability', 'energy', 'speechiness']]
 
-album_feat['track_name'] = album_feat['track_name'].str.replace('- 2015 Remaster', '', regex=True)
+#-----------------------Get Lyrics from Genius API-----------------------#
 
-#----------------------------------------------------------------#
-
+# Use Genius API key with LyricsGenius, remove unnecessary terms
 gns = genius.Genius(open('.genius_token').read(),
                     remove_section_headers=True,
                     skip_non_songs=True,
@@ -52,73 +59,102 @@ gns = genius.Genius(open('.genius_token').read(),
                                       '(Version)', '(Extended)', '(Remaster)',
                                       '(Demo)', '(Writing Session)', '(Outtake)'])
 
-get_lyrics = gns.search_album('Power, Corruption & Lies', 'New Order')
 
+# Search for specific album, save scraped results as JSON
+get_lyrics = gns.search_album('Power, Corruption & Lies', 'New Order')
 get_lyrics.save_lyrics()
 
-#----------------------------------------------------------------#
+#-----------------------Scrub Lyrics for Tokenizer-----------------------#
 
-raw_lyrics = json.load(open('Lyrics_PowerCorruptionLies.json'))
+# Load lyrics JSON
+with open('Lyrics_PowerCorruptionLies.json', 'r') as f:
+    raw_lyrics = json.load(f)
 
+# Specifically seeking lyrics from each album track, initialized as pandas DataFrame
 raw_lyrics = raw_lyrics.get('tracks')
 raw_lyrics = pd.json_normalize(raw_lyrics)
-
 raw_lyrics = raw_lyrics[['song.title', 'song.lyrics']]
 
 def clean_lyrics(text):
+    '''
+    Scraped lyrics from LyricsGenius often still have strings that must be scrubbed.
+
+    This function scrubs unnecessary left over text from this album's lyrics since each
+    word will need to be tokenized later.
+    '''
     text = text.str.replace('by New Order', '',)
     text = text.str.replace('You might also like', '')
+    text = text.str.replace('See New Order LiveGet tickets as low as \$\d+', '', regex=True)
     text = text.str.replace('^.*(?:...)Lyrics', '', regex=True)
     text = text.str.replace('\d+' + 'Embed', '', regex=True)
-    text = text.str.replace('Embed', '', regex=True)
-    text = text.str.replace('\n', ' ', regex=True)
+    text = text.str.replace('Embed', '')
+    text = text.str.replace('\n', ' ')
     text = text.str.strip()
     return text
 
 scrubbed_lyrics = raw_lyrics.apply(clean_lyrics)
 
+# Joining scrubbed lyrics with previous album features DataFrame
 album = album_feat.join(scrubbed_lyrics).rename(columns={'track_name': 'Title',
                                                          'danceability': 'Danceability',
                                                          'energy' : 'Energy',
                                                          'speechiness': 'Speechiness',
                                                          'song.lyrics': 'Lyrics'})
 
+# This column is now redundant given the 'Title' column is there after append
 album = album.drop('song.title', axis=1)
 
-#----------------------------------------------------------------#
+#-----------------------Define Tokenized DataSet Class-----------------------#
 
 class DataSet:
+    '''Defines class DataSet which represents a dataset of tokenized text data.'''
+    
     def __init__(self, token_txt):
+        '''
+        The constructor of this DataSet class takes the argument below.
+
+        token_txt:
+            A dictionary containing tokenized text data
+        '''
         self.token_txt = token_txt
     
     def __len__(self):
+        '''
+        Returns the length of the input_ids component of the token_txt data.
+        '''
         return len(self.token_txt['input_ids'])
     
     def __getitem__(self, idx):
+        '''
+        Returns a dictionary containing the values of the token_txt data at the given index.
+
+        idx:
+            The index of the values to retrieve
+        '''
         return {k: v[idx] for k, v in self.token_txt.items()}
 
-#----------------------------------------------------------------#
+#-----------------------Get & Run Sentiment Model-----------------------#
 
+# Get sentiment model and initialize tokenizer, model, and trainer
 get_model = 'j-hartmann/emotion-english-distilroberta-base'
 tokenizer = AutoTokenizer.from_pretrained(get_model)
 model = AutoModelForSequenceClassification.from_pretrained(get_model)
 trainer = Trainer(model=model)
 
+# Convert lyrics to list and tokenize
 lyrics = album['Lyrics'].tolist()
+token_lyrics = tokenizer(lyrics, truncation=True, padding=True)
 
-token_lyrics = tokenizer(lyrics, 
-                         truncation=True, 
-                         padding=True)
-
+# Derive sentiment predictions via trainer
 input_lyrics = DataSet(token_lyrics)
-
 sentiment = trainer.predict(input_lyrics)
 
-#----------------------------------------------------------------#
+#-----------------------Append Sentiment Scores-----------------------#
 
+# Prepare predictions for final append
 pred = sentiment.predictions.argmax(-1)
 label = pd.Series(pred).map(model.config.id2label)
-score = (np.exp(sentiment[0]) / np.exp(sentiment[0]).sum(-1, keepdims=True))
+scores = (np.exp(sentiment[0]) / np.exp(sentiment[0]).sum(-1, keepdims=True))
 
 anger = []
 disgust = []
@@ -128,15 +164,17 @@ neutral = []
 sadness = []
 surprise = []
 
+# Appends the corresponding emotional score from list of scores to lyrics list
 for i, lyric in enumerate(lyrics):
-    anger.append(score[i][0])
-    disgust.append(score[i][1])
-    fear.append(score[i][2])
-    joy.append(score[i][3])
-    neutral.append(score[i][4])
-    sadness.append(score[i][5])
-    surprise.append(score[i][6])
+    anger.append(scores[i][0])
+    disgust.append(scores[i][1])
+    fear.append(scores[i][2])
+    joy.append(scores[i][3])
+    neutral.append(scores[i][4])
+    sadness.append(scores[i][5])
+    surprise.append(scores[i][6])
 
+# Convert results to DataFrame and rename column headers
 model_result = pd.DataFrame(list(zip(label, anger, disgust, fear, joy,
                                      neutral, sadness, surprise, lyrics)),
                             columns=['Sentiment', 'Anger',
@@ -146,14 +184,16 @@ model_result = pd.DataFrame(list(zip(label, anger, disgust, fear, joy,
 
 model_result['Sentiment'] = model_result['Sentiment'].str.capitalize()
 
-#----------------------------------------------------------------#
+#-----------------------Finalize Resuls DataFrame-----------------------#
 
+# Final merge between sentiment results DataFrame and previous album DataFrame
 final_result = album.merge(model_result, on='Lyrics')
 
+# Shifting the 'Lyrics' and 'Sentiment' columns to better positions for readability
 shift1 = final_result.pop('Lyrics')
 shift2 = final_result.pop('Sentiment')
-
 final_result.insert(11, 'Lyrics', shift1)
 final_result.insert(1, 'Sentiment', shift2)
 
+# Save to CSV
 final_result.to_csv('NewOrder_PCL_Sentiment.csv', index=False)
